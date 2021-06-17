fbdf6a1784dbd95b10b31d9955ce48bbf9b073f5
covid10 <71146231+covid10@users.noreply.github.com>
2021-06-17 10:25:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,114 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const argv = require('./yargs.js');
+const fs = require('fs');
+const request = require('request');
+
+var config = {};
+config.bridgeIp = argv.bridgeIp;
+config.bridgeUser = argv.bridgeUser;
+config.endpoints = argv.endpoints;
+config.backupDir = argv.backupDir || "./" + argv.bridgeUser;
+
+/**
+ * backup endpoint to a local directory
+ * @param {string} backup directory
+ * @param {string} hue endpoint
+ */
+module.exports.backup = (backupDir, endpoint) => {
+
+    if (typeof endpoint === 'undefined') return;
+    
+    const outfile = backupDir + '/' + endpoint + '.json';
+    const url = 'http://' + config.bridgeIp + "/api/" + config.bridgeUser + "/" + endpoint;
+    
+    if (!fs.existsSync(backupDir)) {
+        console.log('creating ' + backupDir);
+        fs.mkdirSync(backupDir);
+    }
+    
+    console.log('getting ' + url);
+    request(url, { method: 'GET', json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        
+        fs.writeFile(outfile, JSON.stringify(body, null, 2), (err) => {  
+            if (err) throw err;
+            console.log('saved ' + outfile);
+        });
+        
+        return nextEndpoint();
+    });
+};
+
+/**
+ * restore endpoint from a local directory
+ * @param {string} backup directory
+ * @param {string} hue endpoint
+ */
+module.exports.restore = (backupDir, endpoint) => {
+    
+    const putResource = (resource) => {
+
+        if (typeof resource === 'undefined') return nextResource();
+        
+        delete resource.owner;
+        delete resource.created;
+        delete resource.lasttriggered;
+        delete resource.timestriggered;
+        
+        console.log('putting ' + (resourceIdx + 1) + '/' + resources.length + ' ' + endpoint + ' to ' + url);
+        request(url, { method: 'PUT', json: true, body: resource}, (err, res, body) => {
+            if (err) { return console.log(err); }
+            console.log(body);
+            return nextResource();
+        });
+    };
+    
+    const nextResource = () => {
+        resourceIdx++;
+        if (resourceIdx >= resources.length) return nextEndpoint();
+        
+        setTimeout( function(res) {
+            putResource(res);
+        }.bind(this, resources[resourceIdx]), 1000);
+        return;
+    };
+    
+    const infile = backupDir + '/' + endpoint + '.json';
+    const url = 'http://' + config.bridgeIp + "/api/" + config.bridgeUser + "/" + endpoint;
+    
+    if (!fs.existsSync(backupDir)) {
+        console.log('backup dir does not exist: ' + backupDir);
+        return;
+    }
+    
+    if (!fs.existsSync(infile)) {
+        console.log('backup file does not exist: ' + infile);
+        return;
+    }
+    
+    const json = JSON.parse(fs.readFileSync(infile, 'utf8'));
+    
+    let resources = [];
+    if (endpoint === 'config')
+        resources = [json];
+    else
+        resources = Object.values(json);
+    
+    let resourceIdx = 0;
+    putResource(resources[resourceIdx]);
+};
+
+const nextEndpoint = () => {
+    endpointIdx++;
+    if (endpointIdx >= config.endpoints.length) return;
+    setTimeout( function(backupDir, endpoint) {
+        this[argv._[0]](backupDir, endpoint);
+    }.bind(this, config.backupDir, config.endpoints[endpointIdx]), 1000);
+    return;
+};
+
+let endpointIdx = 0;
+this[argv._[0]](config.backupDir, config.endpoints[endpointIdx]);
---
+++ yargs.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+'use strict';
+
+module.exports = require('yargs')
+
+    .usage('Usage: $0 <cmd> [options]')
+    
+    .example('$0 backup -b "192.168.86.131" -u <uuid> -d ./my-backup -e rules scenes')
+    .example('$0 restore -d ./my-backup')
+    
+    .alias('h', 'help')
+    .alias('v', 'version')
+    
+    .demandCommand(1,1)
+    .command('backup', 'Backup data to a local directory')
+    .command('restore', 'Restore data from a local directory')
+    
+    .option('c', {
+        config: true,
+        description: 'Path to JSON config file',
+        alias: 'config-file',
+        nargs: 1
+    })
+    
+    .option('b', {
+        string: true,
+        description: 'Hue bridge address:port',
+        alias: 'bridge-ip',
+        nargs: 1,
+        demandOption: true
+    })
+    
+    .option('u', {
+        string: true,
+        description: 'Hue bridge user',
+        alias: 'bridge-user',
+        nargs: 1,
+        demandOption: true
+    })
+    
+    .option('d', {
+        string: true,
+        description: 'Path to Hue backup directory',
+        alias: 'backup-dir',
+        nargs: 1
+    })
+    
+    .option('e', {
+        array: true,
+        description: 'Hue endpoints list',
+        default: ['rules', 'schedules', 'scenes'],
+        alias: 'endpoints',
+        choices: ['config','groups','lights','rules','scenes','schedules','sensors'],
+        demandOption: true
+    })
+    
+    .showHelpOnFail(false, 'Specify --help for available options')
+    .epilog('for more information visit https://github.com/klutchell/hue-bridge-backup')
+    .strict(true)
+    .argv;
